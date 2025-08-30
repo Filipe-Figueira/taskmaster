5ac21f11baa07343fecfe449a2df5c4305ee5be2
Filipe Figueira <filipefigueiraf@gmail.com>
2025-08-30 11:29:29 +0100

    Adição das logos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,28 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TaskMaster - @yield('title')</title>
     <!-- Font Awesome CDN -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
 
+    <!-- Botão para abrir sidebar no mobile -->
     <section class="bordered w-full h-full bg-gray-950/20 flex backdrop-blur-md shadow-lg shadow-black/25">
         <!-- Sidebar -->
-        <nav class="navbar">
+        <nav id="sidebar" class="sidebar">
             <div>
-                <div class="flex items-center gap-3 mb-10">
-                    <h1 class="text-lg font-bold ">TaskMaster</h1>
+                <div class="logo-container">
+                    <img src="{{ asset('images/taskmaster-logo.jpg')}}" alt="tasmaster-logo">
+
+                    <button id="btnMenu">
+                        <i class="fa fa-bars"></i>
+                    </button>
                 </div>
                 <ul class="space-y-2">
                     <li>
-                        <a href="#" class="">
+                        <a href="#">
                             <i class="fa fa-chart-line"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
                     <li>
-                        <a href="{{  route('categories.index') }}">
+                        <a href="{{ route('categories.index') }}">
                             <i class="fa fa-layer-group"></i>
                             <span>Categorias</span>
                         </a>
@@ -52,24 +57,41 @@
                     </li>
                 </ul>
             </div>
-            <div class=" flex flex-col items-center justify-center gap-6">
-                <div class="flex flex-wrap justify-center items-center gap-3 mb-4">
-                    <img src="{{ asset('images/avatar.jpg') }}" alt="avatar" class="w-10 h-10 rounded-full object-cover">
-                    <p class="text-sm"><span class="font-medium ">{{ auth()->user()->name }}</span></p>
-                </div>
-                    <form action="{{ route('auth.logout') }}" method="POST" class="flex items-center justify-center gap-2">
-                    @csrf
 
-                        <button class="btn btn-danger btn-icon"><i class="fa fa-sign-out-alt"></i>Sair</button>
-                    </form>
+            <div class=" flex flex-col items-center justify-center gap-3">
+                <div class="flex flex-col justify-center items-center gap-3 mb-4 text-center">
+                    <img src="{{ asset('images/avatar.jpg') }}" alt="avatar"
+                        class="w-10 h-10 rounded-full object-cover">
+                    <p class="text-sm"><span class="font-medium">{{ auth()->user()->name }}</span></p>
                 </div>
+                <form action="{{ route('auth.logout') }}" method="POST" class="flex items-center justify-center gap-2">
+                    @csrf
+                    <button class="btn btn-danger btn-icon"><i class="fa fa-sign-out-alt"></i>Sair</button>
+                </form>
+            </div>
             </div>
         </nav>
         <!-- Main content -->
-        <main class="w-full p-6">
-             @yield('content')
+        <main class="flex-1 p-6 overflow-y-auto">
+            @yield('content')
         </main>
     </section>
 
+    <script>
+        const btnMenu = document.getElementById('btnMenu');
+        const sidebar = document.getElementById('sidebar');
+        const menu = document.querySelectorAll("#sidebar ul li a");
+
+        btnMenu.addEventListener('click', () => {
+           // sidebar.classList.toggle('-translate-x-full')
+            menu.forEach(i => {
+
+                i.classList.toggle('menu-mobile');
+            });
+            sidebar.classList.toggle('sidebar-mobile')
+        });
+    </script>
+
 </body>
+
 </html>
